4bdd2826e7311a9bd3d2ec682862bd66761b7f6c
Joao <devlambertjoao@gmail.com>
2020-11-30 01:50:21 -0300

    Alterando arquivos cache service worker

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,10 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_02'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-  '/my-basic-react-pwa/',                     // If you have separate JS/CSS files,
-  '/my-basic-react-pwa/index.html'            // add path to those files here
+  '/',                     // If you have separate JS/CSS files,
+  '/index.html',            // add path to those files here
+  'index.html'            // add path to those files here
 ]
 
 // Respond with cached resources
